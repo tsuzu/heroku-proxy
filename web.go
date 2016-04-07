87dfa71e5a1a207c7e5386e5d1ad8aed72994c37
Tsuzu <tsuzu.0623@gmail.com>
2016-04-08 04:02:24 +0900

    Fixed bugs

--- web.go
+++ web.go
@@ -5,25 +5,19 @@ import(
 	"fmt"
 	"os"
     "net/http"
-//    "net/http/httputil"
-//    "net/url"
+    "net/http/httputil"
+    "net/url"
 )
 
 func main() {
     sourceAddress := ":" + os.Getenv("PORT")
-//    destinationUrlString := "http://azure.wt6.pw/"
-//	destinationUrl, _ := url.Parse(destinationUrlString)
-    //proxyHandler := httputil.NewSingleHostReverseProxy(destinationUrl)
+	destinationUrlString := "http://azure.wt6.pw/"
+	destinationUrl, _ := url.Parse(destinationUrlString)
+   	proxyHandler := httputil.NewSingleHostReverseProxy(destinationUrl)
 
-	mux := http.NewServeMux()
-	proxyHandler := func(rw http.ResponseWriter, req *http.Request){
-		fmt.Fprintf(rw, "Hello, world")
-	}
-
-	mux.HandleFunc("/", proxyHandler)
     server := http.Server{
         Addr: sourceAddress,
-        Handler: mux,
+        Handler: proxyHandler,
     }
     err := server.ListenAndServe()
 
