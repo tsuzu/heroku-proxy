cbd0d2eaffef72ae48fba002197fa73d83abd41d
Tsuzu <tsuzu.0623@gmail.com>
2016-04-08 03:53:12 +0900

    Hello server

--- web.go
+++ web.go
@@ -5,18 +5,25 @@ import(
 	"fmt"
 	"os"
     "net/http"
-    "net/http/httputil"
-    "net/url"
+//    "net/http/httputil"
+//    "net/url"
 )
 
 func main() {
     sourceAddress := ":" + os.Getenv("PORT")
-    destinationUrlString := "http://azure.wt6.pw/"
-    destinationUrl, _ := url.Parse(destinationUrlString)
-    proxyHandler := httputil.NewSingleHostReverseProxy(destinationUrl)
+//    destinationUrlString := "http://azure.wt6.pw/"
+//	destinationUrl, _ := url.Parse(destinationUrlString)
+    //proxyHandler := httputil.NewSingleHostReverseProxy(destinationUrl)
+
+	mux := http.NewServeMux()
+	proxyHandler := func(rw http.ResponseWriter, req *http.Request){
+		fmt.Fprintf(rw, "Hello, world")
+	}
+
+	mux.HandleFunc("/", proxyHandler)
     server := http.Server{
         Addr: sourceAddress,
-        Handler: proxyHandler,
+        Handler: mux,
     }
     err := server.ListenAndServe()
 
